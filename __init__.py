15299c71b5c28e376f2ea1d4432357480415988b
kappa54 <z3xcvv@gmail.com>
2024-01-29 01:14:43 +0900

    LoadImageDedup - overwrite unconditionally

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
-
+WEB_DIRECTORY = "./js"
--- nodes.py
+++ nodes.py
@@ -10,36 +10,54 @@ from pathlib import Path
 import hashlib
 
 
-class LoadImageDedup:
+class KLoadImageDedup:
     @classmethod
     def INPUT_TYPES(s):
         input_dir = folder_paths.get_input_directory()
         files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
-        return {"required":
-                    {
-                        "image": (sorted(files), {
-                            "image_upload": True}),
-                    },
-                }
+        return {
+            "required": {
+                    "image": (sorted(files), {
+                                "image_upload_dedup": True
+                            }),
+            },
+            "optional": {
+            },
+        }
 
     CATEGORY = "image"
 
     RETURN_TYPES = ("IMAGE", "MASK")
     FUNCTION = "load_image"
     def load_image(self, image):
-        image_path = folder_paths.get_annotated_filepath(image)
-        output_image, output_mask = self.__do_load(image_path)
+        image_path = Path(folder_paths.get_annotated_filepath(image))
+        image_fn = image_path.name
+        im = Image.open(str(image_path))
+
+        output_image, output_mask = self.__do_load(image_path, pil_im=im)
 
         return (output_image, output_mask)
 
+    def __detect_duplicate(self, fp1, fp2):
+        def do_hash(fp):
+            m = hashlib.sha256()
+            with open(fp, 'rb') as f:
+                m.update(f.read())
+            return m.digest().hex()
+        h1 = do_hash(fp1)
+        h2 = do_hash(fp2)
+        return h1 == h2
 
-    def __do_load(self, img_path):
+    def __do_load(self, img_path, pil_im=None):
         """
             Reference implementation: nodes.LoadImage.load_image
             https://github.com/comfyanonymous/ComfyUI/blob/7f4725f6b3f72dd8bdb60dae5dd2c3e943263bcf/nodes.py#L1454
         """
         img_path = Path(img_path)
-        im_original = Image.open(str(img_path))
+        if pil_im is not None:
+            im_original = pil_im
+        else:
+            im_original = Image.open(str(img_path))
         output_imgs = []
         output_masks = []
         for im_single in ImageSequence.Iterator(im_original):
@@ -74,108 +92,19 @@ class LoadImageDedup:
         return m.digest().hex()
 
     @classmethod
-    def VALIDATE_INPUTS(self, input_image_path):
+    def VALIDATE_INPUTS(self, image):
         if not folder_paths.exists_annotated_filepath(image):
             return "Invalid input image: '{}'".format(image)
         return True
 
 
-class ExampleNode:
-    """
-    Class methods
-    -------------
-    Attributes
-    ----------
-    RETURN_TYPES (`tuple`): 
-        The type of each element in the output tulple.
-    RETURN_NAMES (`tuple`):
-        Optional: The name of each output in the output tulple.
-    FUNCTION (`str`):
-        The name of the entry-point method. For example, if `FUNCTION = "execute"` then it will run Example().execute()
-    OUTPUT_NODE ([`bool`]):
-        If this node is an output node that outputs a result/image from the graph. The SaveImage node is an example.
-        The backend iterates on these output nodes and tries to execute all their parents if their parent graph is properly connected.
-        Assumed to be False if not present.
-    CATEGORY (`str`):
-        The category the node should appear in the UI.
-    execute(s) -> tuple || None:
-        The entry point method. The name of this method must be the same as the value of property `FUNCTION`.
-        For example, if `FUNCTION = "execute"` then this method's name must be `execute`, if `FUNCTION = "foo"` then it must be `foo`.
-    """
-    def __init__(self):
-        pass
-    
-    @classmethod
-    def INPUT_TYPES(s):
-        """
-            Return a dictionary which contains config for all input fields.
-            Some types (string): "MODEL", "VAE", "CLIP", "CONDITIONING", "LATENT", "IMAGE", "INT", "STRING", "FLOAT".
-            Input types "INT", "STRING" or "FLOAT" are special values for fields on the node.
-            The type can be a list for selection.
-
-            Returns: `dict`:
-                - Key input_fields_group (`string`): Can be either required, hidden or optional. A node class must have property `required`
-                - Value input_fields (`dict`): Contains input fields config:
-                    * Key field_name (`string`): Name of a entry-point method's argument
-                    * Value field_config (`tuple`):
-                        + First value is a string indicate the type of field or a list for selection.
-                        + Secound value is a config for type "INT", "STRING" or "FLOAT".
-        """
-        return {
-            "required": {
-                "image": ("IMAGE",),
-                "int_field": ("INT", {
-                    "default": 0, 
-                    "min": 0, #Minimum value
-                    "max": 4096, #Maximum value
-                    "step": 64, #Slider's step
-                    "display": "number" # Cosmetic only: display as "number" or "slider"
-                }),
-                "float_field": ("FLOAT", {
-                    "default": 1.0,
-                    "min": 0.0,
-                    "max": 10.0,
-                    "step": 0.01,
-                    "round": 0.001, #The value represeting the precision to round to, will be set to the step value by default. Can be set to False to disable rounding.
-                    "display": "number"}),
-                "print_to_screen": (["enable", "disable"],),
-                "string_field": ("STRING", {
-                    "multiline": False, #True if you want the field to look like the one on the ClipTextEncode node
-                    "default": "Hello World!"
-                }),
-            },
-        }
-
-    RETURN_TYPES = ("IMAGE",)
-    #RETURN_NAMES = ("image_output_name",)
-
-    FUNCTION = "test"
-
-    #OUTPUT_NODE = False
-
-    CATEGORY = "Example"
-
-    def test(self, image, string_field, int_field, float_field, print_to_screen):
-        if print_to_screen == "enable":
-            print(f"""Your input contains:
-                string_field aka input text: {string_field}
-                int_field: {int_field}
-                float_field: {float_field}
-            """)
-        #do some processing on the image, in this example I just invert it
-        image = 1.0 - image
-        return (image,)
-
-
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
-    "LoadImageDedup": LoadImageDedup,
-    "ExampleNode": ExampleNode,
+    "LoadImageDedup": KLoadImageDedup,
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
     "LoadImageDedup": "Load Image Dedup",
-    "ExampleNode": "Example Node"
 }
